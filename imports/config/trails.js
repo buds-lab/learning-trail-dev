2160a922db3d456b85edcbcfdff1c042fa5a18ac
nbiton <naor.biton@gmail.com>
2019-01-20 15:17:19 +0800

    feat: added trails config and one mock station cover photo

---
+++ imports/config/trails.js
@@ -0,0 +1,14 @@
+export const trailsDefs = [
+  {
+    name: 'BIOPHILIC TRAIL',
+    stations: [
+      'BIOPHILIC',
+      'NATURE IN PLACE',
+      'NATURE OF PLACE',
+      'MATERIALITY',
+      'FUTURE TECHNOLOGIES 1',
+      'FUTURE TECHNOLOGIES 2',
+      'FUTURE TECHNOLOGIES 3'
+    ]
+  }
+]
