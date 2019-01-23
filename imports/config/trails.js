3cdd62283d4464e8ce0cfc4305f2750e58bb64d9
nbiton <naor.biton@gmail.com>
2019-01-23 19:28:46 +0800

    feat: added all trails and stations to the config file

--- imports/config/trails.js
+++ imports/config/trails.js
@@ -10,5 +10,67 @@ export const trailsDefs = [
       'FUTURE TECHNOLOGIES 2',
       'FUTURE TECHNOLOGIES 3'
     ]
+  },
+  {
+    name: 'WATER TRAIL',
+    stations: [
+      'WATER TRAIL',
+      'ABC WATER PROGRAMME',
+      'WATER FEATURE',
+      'BIO RETENTION BASIN',
+      'FUTURE TECHNOLOGIES 1',
+      'FUTURE TECHNOLOGIES 2',
+      'FUTURE TECHNOLOGIES 3'
+    ]
+  },
+  {
+    name: 'WELLNESS TRAIL',
+    stations: [
+      'WELLNESS TRAIL',
+      'LIGHTING',
+      'AIR',
+      'SUSTAINABLE NUS',
+      'MOVEMENT',
+      'FUTURE TECHNOLOGIES 1',
+      'FUTURE TECHNOLOGIES 2',
+      'FUTURE TECHNOLOGIES 3'
+    ]
+  },
+  {
+    name: 'TROPICAL ARCHITECTURE TRAIL',
+    stations: [
+      'TROPICAL ARCHITECTURE TRAIL',
+      'BUILDING FORM',
+      'ENVELOPE DESIGN',
+      'NATURAL VENTILATION',
+      'LANDSCAPE DESIGN',
+      'FUTURE TECHNOLOGIES 1',
+      'FUTURE TECHNOLOGIES 2',
+      'FUTURE TECHNOLOGIES 3'
+    ]
+  },
+  {
+    name: 'HYBRID COOLING TRAIL',
+    stations: [
+      'HYBRID COOLING TRAIL',
+      'ADAPTIVE COMFORT',
+      'MIXED MODE',
+      'ENHANCED IEQ',
+      'FUTURE TECHNOLOGIES 1',
+      'FUTURE TECHNOLOGIES 2',
+      'FUTURE TECHNOLOGIES 3'
+    ]
+  },
+  {
+    name: 'NET ZERO ENERGY TRAIL',
+    stations: [
+      'NET ZERO ENERGY TRAIL',
+      'ENERGY GENERATION',
+      'DEMAND REDUCTION',
+      'CO WORKING',
+      'FUTURE TECHNOLOGIES 1',
+      'FUTURE TECHNOLOGIES 2',
+      'FUTURE TECHNOLOGIES 3'
+    ]
   }
 ]
