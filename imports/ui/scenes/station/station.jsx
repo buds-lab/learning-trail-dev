9705c26cde55997e87ed20733024f1d457ef46f3
nbiton <naor.biton@gmail.com>
2019-01-26 22:22:10 +0800

    feat: refactored "romanize" to util module

--- imports/ui/scenes/station/station.jsx
+++ imports/ui/scenes/station/station.jsx
@@ -15,21 +15,9 @@ import Button from '@material-ui/core/Button'
 import { SiteTabbedLayout, InfoTab, FeedbackPanel } from 'meteor/buds-shared-meteor-ui'
 import { trailsDefs } from '/imports/config/trails'
 import StationPunchcards, { collectionName } from '/imports/api/station-punchcards'
+import { romanize } from '/imports/util/formatters'
 import StationCharter from '../../components/station-charter'
 
-// from: https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
-function romanize (num) {
-  if (isNaN(num)) return NaN
-  const digits = String(+num).split('')
-  const key = [
-    '', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM', '', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX',
-    'LXXX', 'XC', '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'
-  ]
-  let roman = ''
-  let i = 3
-  while (i--) roman = (key[+digits.pop() + (i * 10)] || '') + roman
-  return Array(+digits.join('') + 1).join('M') + roman
-}
 
 const snakeCase = str => str.split(' ').join('_')
 const desnakeCase = str => str.split('_').join(' ')
